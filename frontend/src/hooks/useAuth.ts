0eab37a1ae1705bb0c7b4f667306d552483b92c5
Danylo Khobta <danylokhobta@gmail.com>
2025-05-07 17:38:58 +0200

    fix

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,10 @@
 import { useDispatch } from "react-redux";
-import { setAccessToken, setIsAuthenticated } from "../store/authSlice";
+import { setIsAuthenticated } from "../store/authSlice";
 
 const useAuth = () => {
   const dispatch = useDispatch();
 
   return {
-    setAccessToken: (token: string) => dispatch(setAccessToken(token)),
     setIsAuthenticated: (status: boolean) => dispatch(setIsAuthenticated(status)),
   };
 };
