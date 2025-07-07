7574ab19ecfa4bc0a380278914fddacdeafdf2ec
Danylo Khobta <danylokhobta@gmail.com>
2025-07-07 19:44:11 +0200

    fix

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,10 +12,9 @@ async function bootstrap() {
     origin: [
       'http://localhost:3000',
       'https://task-manager-xrli.onrender.com',
-      'http://localhost:5000',
-      'http://localhost:5173',
     ],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
   app.use(cookieParser());
   app.use(json());
